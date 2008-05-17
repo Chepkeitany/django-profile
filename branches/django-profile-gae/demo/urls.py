9918b879fc3b76a704b649e3ce6832ac9a080cf8
david.rubert <david.rubert@7b2eb738-463f-0410-8820-43b14e3dcc91>
2008-05-17 10:51:29 +0000

    Google AppEngine branch

---
+++ branches/django-profile-gae/demo/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+
+    # Demo FrontPage
+    (r'^$', "demo.views.front", {'template': 'front.html' }),
+
+    # Account application
+    (r'^accounts/', include('account.urls')),
+
+    # Profile application
+    (r'^profile/', include('userprofile.urls')),
+
+    # Serves media content. WARNING!! Only for development uses. On production use lighthttpd for media content.
+    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '../media/'}),
+
+
+    # Admin (not really needed)
+    (r'^admin/', include('django.contrib.admin.urls')),
+
+)
