0013d3da8ea57f834ff0f9fd5c3e88c94e47b834
david.rubert <david.rubert@7b2eb738-463f-0410-8820-43b14e3dcc91>
2008-05-18 15:31:53 +0000

    More changes

--- branches/django-profile-gae/urls.py
+++ branches/django-profile-gae/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls.defaults import *
+from google.appengine.api import users
 from django.views.generic.simple import direct_to_template
 from userprofile.models import Profile
 
 urlpatterns = patterns('',
     # Demo FrontPage
-    (r'^$', direct_to_template, {'extra_context': { 'profiles': Profile.all() }, 'template': 'front.html' }),
+    (r'^$', direct_to_template, {'extra_context': { 'user': users.get_current_user(), 'profiles': Profile.all() }, 'template': 'front.html' }),
 
     # Profile application
     (r'^profile/', include('userprofile.urls')),
