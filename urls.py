07b043a1152293ad0750ece4199293a3522b409f
david.rubert <david.rubert@7b2eb738-463f-0410-8820-43b14e3dcc91>
2008-05-23 09:50:54 +0000

    Some bugs solved

--- urls.py
+++ urls.py
@@ -3,9 +3,12 @@ from google.appengine.api import users
 from django.views.generic.simple import direct_to_template
 from userprofile.models import Profile
 
+def get_current_user():
+    return users.get_current_user()
+
 urlpatterns = patterns('',
     # Demo FrontPage
-    (r'^$', direct_to_template, {'extra_context': { 'user': users.get_current_user(), 'profiles': Profile.all() }, 'template': 'front.html' }),
+    (r'^$', direct_to_template, {'extra_context': { 'user': get_current_user(), 'profiles': Profile.all() }, 'template': 'front.html' }),
 
     # Profile application
     (r'^profile/', include('userprofile.urls')),
