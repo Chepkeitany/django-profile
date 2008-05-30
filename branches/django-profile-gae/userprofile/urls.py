a47810871427018a292b20d068739077ff8a42f4
david.rubert <david.rubert@7b2eb738-463f-0410-8820-43b14e3dcc91>
2008-05-30 14:34:41 +0000

    Public information selection working, and some bugs solved.

--- branches/django-profile-gae/userprofile/urls.py
+++ branches/django-profile-gae/userprofile/urls.py
@@ -3,13 +3,16 @@ from django.views.generic.simple import direct_to_template
 from userprofile.views import *
 from django.conf import settings
 
+def get_current_user():
+    return users.get_current_user()
+
 urlpatterns = patterns('',
     # Private profile
     (r'^$', private, {'APIKEY': settings.APIKEY, 'template': 'userprofile/private.html'}),
     (r'^save/$', save),
     (r'^logout/$', logout),
     (r'^delete/$', delete, {'template': 'userprofile/delete.html'}),
-    (r'^delete/done/$', direct_to_template, {'template': 'userprofile/delete_done.html'}),
+    (r'^delete/done/$', direct_to_template, {'extra_context': { 'user': get_current_user }, 'template': 'userprofile/delete_done.html'}),
     (r'^avatar/delete/$', avatarDelete),
     (r'^fill/(?P<model>[a-z]+)/$', fill),
     (r'^avatar/delete/(?P<key>[0-9a-zA-Z]+)/$', avatarDelete),
