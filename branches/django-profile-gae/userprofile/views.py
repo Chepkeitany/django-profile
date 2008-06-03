3de2f07e59dbd6a5369d8c161a917c93c6b3df27
david.rubert <david.rubert@7b2eb738-463f-0410-8820-43b14e3dcc91>
2008-06-03 16:26:44 +0000

    More changes

--- branches/django-profile-gae/userprofile/views.py
+++ branches/django-profile-gae/userprofile/views.py
@@ -130,8 +130,8 @@ def delete(request, template):
 @login_required
 def searchflickr(request, template):
     if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest' and request.method=="POST" and request.POST.get('search'):
-        photos = flickr.photos_search(tags=request.POST.get('search'))
-        return HttpResponse(simplejson.dumps({'success': True}))
+        photos = [ photo.getURL(size='Square', urlType='source') for photo in flickr.photos_search(tags=request.POST.get('search'))[:10] ]
+        return HttpResponse(simplejson.dumps({'success': True, 'photos': photos}))
     else:
         return render_to_response(template, locals(), context_instance=RequestContext(request))
 
