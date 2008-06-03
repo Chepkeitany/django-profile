e9f85b5a0e659aa03f4e1d9e254eb15ecde24de1
david.rubert <david.rubert@7b2eb738-463f-0410-8820-43b14e3dcc91>
2008-06-03 16:18:45 +0000

    Better avatar functionality

--- branches/django-profile-gae/settings.py
+++ branches/django-profile-gae/settings.py
@@ -34,6 +34,8 @@ APIKEY="ABQIAAAA06IJoYHDPFMx4u3hTtaghxTgGHawVmFK-PeSfwKit_FZRDle2xRzLAGoZFdyMnOf
 
 FLICKR_APIKEY="4a2403a843e91a18eb6f3264d2c6489d"
 
+MIMETYPES = { 'jpg': 'image/jpeg', 'jpeg': 'image/jpeg', 'gif': 'image/gif', 'png': 'image/png', }
+
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'appengine'  # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
--- branches/django-profile-gae/userprofile/forms.py
+++ branches/django-profile-gae/userprofile/forms.py
@@ -1,6 +1,7 @@
 from google.appengine.ext.db import djangoforms
 from django import newforms as forms
 from django.utils.translation import ugettext as _
+from django.conf import settings
 from userprofile.models import Profile
 
 class ProfileForm(djangoforms.ModelForm):
@@ -16,15 +17,25 @@ class AvatarForm(forms.Form):
     """
     The avatar form requires only one image field.
     """
-    photo = forms.FileField()
+    photo = forms.FileField(required=False)
+    url = forms.URLField(required=False)
 
     def clean_photo(self):
-        ext_mimetypes = { 'jpg': 'image/jpeg', 'gif': 'image/gif', 'png': 'image/png', }
         photo = self.cleaned_data['photo']
-        if not photo.filename.split(".")[-1].lower() in ext_mimetypes.keys():
+        if photo and not photo.filename.split(".")[-1].lower() in settings.MIMETYPES.keys():
             raise forms.ValidationError(_('The file type is invalid: %s' % type))
 
         return photo
+    
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        return url
+
+    def clean(self):
+        if not (self.cleaned_data.get('photo') or self.cleaned_data.get('url') or self.cleaned_data.get('flickr')):
+            raise forms.ValidationError(_('You must enter one of the 3 possible options'))
+        else:
+            return self.cleaned_data
 
 
 class AvatarCropForm(forms.Form):
